e0e4260f377c8df00897e83ab515504f6e9f971b
Kateryna <k.khalimovska@gmail.com>
2021-02-02 20:38:49 +0200

    filter

--- build/js/common.js
+++ build/js/common.js
@@ -1,7 +1,7 @@
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
-  x = document.getElementsByClassName("course__wrapper");
+  x = document.getElementsByClassName("courses__wrapper");
   if (c == "all") c = "";
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
@@ -36,8 +36,8 @@ var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    var current = document.getElementsByClassName("btn-filter--active");
+    current[0].className = current[0].className.replace(" btn-filter--active", "");
+    this.className += " btn-filter--active";
   });
 }
--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,7 @@
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
-  x = document.getElementsByClassName("course__wrapper");
+  x = document.getElementsByClassName("courses__wrapper");
   if (c == "all") c = "";
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
